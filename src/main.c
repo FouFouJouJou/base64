67309984cf026ccd0923a92d8faa1b25cc4a92d4
foufou <foufou.joujou@gmail.com>
2025-10-28 16:23:42 +0100

    base64 encoding only with printfs

--- src/main.c
+++ src/main.c
@@ -7,12 +7,16 @@
 
 char value_to_encoding(int value) {
   assert(value >= 0 && value <= 63);
-  if (value >= 0 && value < 26) {
+  if (value >= 0 && value <= 25) {
     return 'A'+value;
   }
 
-  if (value >= 26 && value < 52) {
+  if (value >= 26 && value <= 51) {
     return 'a'+(value-26);
+  }
+
+  if (value >= 52 && value <= 61) {
+    return '0'+(value-52);
   }
 
   if (value == 62) {
@@ -21,11 +25,54 @@ char value_to_encoding(int value) {
   if (value == 63) {
     return '-';
   }
+  printf("%d\n", value);
   assert(0 && "UNREACHABLE");
 }
 
+void encode_block(char *string) {
+  int p1 = string[0]>>2;
+  int p2 = (string[0] & 0x03)<<4 | (string[1]>>4);
+  int p3 = (string[1] & 0x0F)<<2 | (string[2]>>6);
+  int p4 = string[2] & 0x3F;
+  printf("%c%c%c%c", value_to_encoding(p1), value_to_encoding(p2), value_to_encoding(p3), value_to_encoding(p4));
+}
+
+void encode_one_padding(char *string) {
+  int p1 = string[0]>>2;
+  int p2 = (string[0] & 0x03)<<4 | (string[1]>>4);
+  int p3 = (string[1] & 0x0F)<<2 ;
+  printf("%c%c%c=", value_to_encoding(p1), value_to_encoding(p2), value_to_encoding(p3));
+}
+
+void encode_two_paddings(char *string) {
+  int p1 = string[0]>>2;
+  int p2 = (string[0] & 0x03)<<4;
+  printf("%c%c==", value_to_encoding(p1), value_to_encoding(p2));
+}
+
 char *encode(char *string) {
-  (void) string;
+  size_t string_size = strlen(string);
+  int total_bits = string_size*8;
+  int missing_bits = total_bits%24;
+  int total_padding = total_bits%3;
+  int total_complete_blocks = total_bits/24;
+  int idx=0;
+  printf("total bits: %d\n", total_bits);
+  printf("missing bits: %d\n", missing_bits);
+  printf("total padding: %d\n", total_padding);
+  printf("total complete blocks: %d\n", total_complete_blocks);
+  for (int i=0; i<total_complete_blocks; ++i) {
+    encode_block(string+idx);
+    idx+=3;
+  }
+
+  if (total_padding == 1) {
+    encode_one_padding(string+idx);
+  }
+  else if (total_padding == 2) {
+    encode_two_paddings(string+idx);
+  }
+  printf("\n");
   return 0;
 }
 
@@ -37,6 +84,7 @@ int main() {
   assert(value_to_encoding(26) == 'a');
   assert(value_to_encoding(34) == 'i');
   assert(value_to_encoding(62) == '+');
-  assert(strncmp(encode(INPUT), ENCODED_INPUT, strlen(ENCODED_INPUT)) == 0);
+  printf("encoding: %s\n", encode(INPUT));
+  // assert(strncmp(encode(INPUT), ENCODED_INPUT, strlen(ENCODED_INPUT)) == 0);
   return EXIT_SUCCESS;
 }
